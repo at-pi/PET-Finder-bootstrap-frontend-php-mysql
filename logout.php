744244cc14f72f5c61037cdebf60fe4f3f2a649a
Aditya Tiwari <31739160+at-pi@users.noreply.github.com>
2018-11-14 10:29:39 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();  
-session_destroy();
-echo "Successfully logged out....................Redirecting to home page in 2 seconds";
-	  header( "refresh:2;url=index.html" );
-?>
